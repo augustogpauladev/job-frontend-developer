c4b6542ff7bbc869d532eece975a86e72d600bb5
Brandon Alves <brandonalvesn@gmail.com>
2021-09-09 22:18:38 -0300

    todos os elementos do html ja estao interados com o js

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const apiKey = 'AIzaSyCDx-33kv3QDIPCC--8ZnHzJgGGcYFK4oI';
+const urlBase = 'https://www.googleapis.com/youtube/v3/search';
+const btn = document.getElementById('#btn');
+const title = document.querySelector('.title');
+const results = querySelector('.results');
+
+
