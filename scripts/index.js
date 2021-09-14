60d7cb6c2a1db2ca86a2994ac0252ba2a1779301
Brandon Alves <brandonalvesn@gmail.com>
2021-09-13 23:46:29 -0300

    comentando codigos

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-const btn = document.querySelector('.btn');
-const results = document.querySelector('.results');
-const info = document.querySelector('.informations');
+const btn = document.querySelector('.btn'); //botao que chama as funcoes
+const results = document.querySelector('.results');//section onde vai aparecer os videos
+const info = document.querySelector('.informations');//tag onde ira aparecer a imagem da bada e as informaçoes
 
- function search(event){
+ const search = (event) => {  //funçao invocadora da api do youtube
 
-  event.preventDefault()
+  event.preventDefault();
     const apiKey = 'AIzaSyCDx-33kv3QDIPCC--8ZnHzJgGGcYFK4oI';
-    const input = document.querySelector('.search').value;
-     info.innerHTML = '';
-     results.innerHTML = '';
+    const input = document.querySelector('.search').value;//aqui pega os dados do input
+     info.innerHTML = '';//limpando busca anterior se houver
+     results.innerHTML = '';//limpando busca anterior se houver
     let result= [];
    
      fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&videoEmbeddable=true&videoSyndicated=true&videoLicense=creativeCommon&key=${apiKey}&type=video&q=${input}`)
@@ -51,18 +51,18 @@ const info = document.querySelector('.informations');
         })  
 };
 
-function informations(band){
+const informations = (band) => {  //funçao invocadora da api TicketMaster
  
   const article = document.createElement('article');
   article.setAttribute('class', 'information');
-  let information = '';
+  let information = '';//sempre q invocada limpa a busca anterior
   info.innerHTML= '<div class=loading ></div>'
 
-  fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${band}&apikey=uwk3vzZlQOho8rBXJHJdT9AE8Amf767z`)
+  fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${band}&apikey=uwk3vzZlQOho8rBXJHJdT9AE8Amf767z`)//requisitando a api
   .then(response => response.json())
-    .then((data) => {     
-      information = data._embedded.attractions[0]
-      article.innerHTML = `
+  .then((data) => {     
+      information = data._embedded.attractions[0]//dados filtrados logo abaixo construçao do html dinamico
+      article.innerHTML = ` 
                           <div class=imageInformation>
                               <img src='${information.images[6].url}' alt='${information.id}'/>
                           </div>
@@ -71,33 +71,49 @@ function informations(band){
                                 <p>
                                   <span style='font-weight:bold'>Nome: </span>${information.name}
                                 </p>
+
                                 <p>
                                    <span style='font-weight:bold'>Gênero: </span>${information.classifications[0].genre.name}
                                 </p>
-                                 <p>
+
+                                <p>
                                     <span style='font-weight:bold'>Segmento: </span>${information.classifications[0].segment.name}
-                                 </p>
+                                </p>
 
                                   <div class='social'>
                                       <p style='font-weight:bold'>Redes sociais e contato:</p>
+
                                       <ul>
-                                          <li><span style='font-weight=bold'>Facebook: </span><br><a href='https://www.facebook.com/${(information.name).replace(" ","")}' target='blank'>https://www.facebook.com/${information.name}</a></li>
-                                          <li><span style='font-weight=bold'>Twitter: </span><br><a href='https://twitter.com/${(information.name).replace(" ","")}' target='blank'>https://twitter.com/${information.name}</a></li>
-                                          <li><span style='font-weight=bold'>Instagram: </span><br><a href='https://www.instagram.com/${(information.name).replace(" ","")}' target='blank'>https://twitter.com/${information.name}</a></li>
-                                          <li><span style='font-weight=bold'>YouTube: </span><br><a href='https://www.youtube.com/user/${(information.name).replace(" ","")}' target='blank'>https://www.youtube.com/user/${information.name}</a></li>
+                                          <li>
+                                            <span style='font-weight=bold'>Facebook: </span><br>
+                                            <a href='https://www.facebook.com/${(information.name).replace(" ","")}' target='blank'>https://www.facebook.com/${information.name}</a>
+                                          </li>
+
+                                          <li>
+                                            <span style='font-weight=bold'>Twitter: </span><br>
+                                            <a href='https://twitter.com/${(information.name).replace(" ","")}' target='blank'>https://twitter.com/${information.name}</a>
+                                          </li>
+
+                                          <li>
+                                            <span style='font-weight=bold'>Instagram: </span><br>
+                                            <a href='https://www.instagram.com/${(information.name).replace(" ","")}' target='blank'>https://twitter.com/${information.name}</a>
+                                          </li>
+                                          <li><span style='font-weight=bold'>YouTube: </span><br>
+                                            <a href='https://www.youtube.com/user/${(information.name).replace(" ","")}' target='blank'>https://www.youtube.com/user/${information.name}</a>
+                                          </li>
                                        
                                        </ul>
                                  </div>`;
          
-          info.innerHTML='';   
-      info.appendChild(article);
+      info.innerHTML='';   //retirando icone de loading ou erro se houver
+      info.appendChild(article);//adicionando conteudo ao html
       
-    }).catch((error)=>{
-      info.innerHTML = '<h4 class = "error"><i class="fas fa-exclamation-circle"> </i>informações adicionais da sua busca não encontradas, tente outro termo de busca!</h4>'
-      setTimeout(()=>info.innerHTML = '<h3 style="color:red"><i class="fas fa-exclamation-circle"> </i></h3>',10000)
     })
-    
+    .catch((error) => {
+      info.innerHTML = '<h4 class = "error"><i class="fas fa-exclamation-circle"> </i>informações adicionais da sua busca não encontradas, tente outro termo de busca!</h4>'
+      console.log(error);
+      setTimeout(()=>info.innerHTML = '<h3 style="color:red"><i class="fas fa-exclamation-circle"> </i></h3>',10000)
+    });
+};
 
-}
-
-btn.addEventListener('click', search);
+btn.addEventListener('click', search);//evento de clique do botao
